c9d65c37b6981cfadd6a3bf4d63d6a253c9c66d7
Deniz Gökçin <dgokcin@gmail.com>
2019-04-24 22:24:42 +0300

    use BeautifulSoup, start extraction

--- src/html_parser.py
+++ src/html_parser.py
@@ -1,37 +1,59 @@
-import urllib
 from collections import OrderedDict
+from bs4 import BeautifulSoup
+import requests
 
-class HTMLExport():
+class CalendarDate():
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+        self.today = []
+
+class TVShow():
+    def __init__(self, title, season, episode):
+        self.title = title
+        self.season = season
+        self.episode = episode
+
+
+class HTMLParser():
 
     calendar = OrderedDict()
-    calendar = {"01-2019": "january.html",
-                "02-2019": "february.html",
-                "03-2019": "march.html",
-                "04-2019": "april.html",
-                "05-2019": "may.html",
-                "06-2019": "june.html",
-                "07-2019": "july.html",
-                "08-2019": "august.html",
-                "09-2019": "september.html",
-                "10-2019": "october.html",
-                "11-2019": "november.html",
-                "12-2019": "december.html"
+    calendar = {
+                #"01-2019": "january.html",
+                #"02-2019": "february.html",
+                #"03-2019": "march.html",
+                "04-2019": "april.html"
+                #"05-2019": "may.html",
+                #"06-2019": "june.html",
+                #"07-2019": "july.html",
+                #"08-2019": "august.html",
+                #"09-2019": "september.html",
+                #"10-2019": "october.html",
+                #"11-2019": "november.html",
+                #"12-2019": "december.html"
                 }
+    def get_tv_show_data(self, page):
+        soup = BeautifulSoup(page.content, 'html.parser')
+        month = soup.find_all('td', class_='SimklTVCalendarColumn')
+        for day in month:
+            episodes = day.find(class_="openLogin")
 
-    def export_to_html(self):
+
+
+    def get_html(self):
         link = "https://simkl.com/tv/calendar"
         calendar_id = "1437817"
 
-        for key, value in HTMLExport.calendar.iteritems():
-            print ("Extracting html info for " + key)
-            # instantiate the parser and fed it some HTML
-            #urllib.urlretrieve("https://simkl.com/tv/calendar/04-2019/1437817/", "test_april.html")
-            urllib.urlretrieve(link + "/" + key + "/" + str(calendar_id), "html/" + value)
+        for key, value in HTMLParser.calendar.iteritems():
+            page = requests.get(link + "/" + key + "/" + str(calendar_id), "html/" + value)
+            self.get_tv_show_data(page)
+
 
 
 def main():
-    exporter = HTMLExport()
-    exporter.export_to_html()
+    exporter = HTMLParser()
+    exporter.get_html()
 
 
 
