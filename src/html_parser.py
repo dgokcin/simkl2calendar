ce7742d3fcf938a4ef62b77bacbaeed4a952ad05
Deniz Gökçin <dgokcin@gmail.com>
2019-04-28 13:54:32 +0300

    huge progress, annoying duplicates

--- src/html_parser.py
+++ src/html_parser.py
@@ -1,6 +1,13 @@
-from collections import OrderedDict
+from __future__ import print_function
 from bs4 import BeautifulSoup
 import requests
+import datetime
+from datetime import timedelta
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 class CalendarDate():
     def __init__(self, day, month, year, str_day):
@@ -22,10 +29,13 @@ class HTMLParser():
     def __init__(self):
         self.link = "https://simkl.com"
         self.calendar_id = "1437817"
+        self.all_events = []
+        self.k = ""
 
     calendar = ["01-2019", "02-2019", "03-2019", "04-2019",
                 "05-2019", "06-2019", "07-2019", "08-2019",
                 "09-2019", "10-2019", "11-2019", "12-2019"]
+
 
     def get_tv_show_data(self, page):
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -50,11 +60,59 @@ class HTMLParser():
                     episode = str(t.find_all(class_='SimklTVCalendarDayListLink')[1].get_text().split()[4])
                     show_id = str(t.find_all(class_='SimklTVCalendarDayListLink')[0].find('a')['href'])
                     fixed_time = self.get_time(show_id)
+                    end_time = str((int(fixed_time.split(':')[0]) + 1) % 24)
+
+                    datetime_object_start = datetime.datetime(int(year), int(month), int(day),
+                                                        int(fixed_time.split(':')[0]), int(fixed_time.split(':')[1]))
+                    datetime_object_end = datetime_object_start + timedelta(hours=1)
+
+                    datetime_object_start_str = datetime_object_start.strftime("%Y-%m-%dT%H:%M:%S-01:00")
+                    datetime_object_end_str = datetime_object_end.strftime("%Y-%m-%dT%H:%M:%S-01:00")
 
                     s = TVShow(show_name, season, episode, fixed_time)
-                    calendar_day.today.append(s)
+                    self.k = {
+                        'summary': 'Google I/O 2015',
+                        'location': '800 Howard St., San Francisco, CA 94103',
+                        'description': 'A chance to hear more about Google\'s developer products.',
+                        'start': {
+                            'dateTime': '2015-05-28T09:00:00-07:00',
+                            'timeZone': 'America/Los_Angeles',
+                        },
+                        'end': {
+                            'dateTime': '2015-05-28T17:00:00-07:00',
+                            'timeZone': 'America/Los_Angeles',
+                        },
+                        'recurrence': [
+                            'RRULE:FREQ=DAILY;COUNT=2'
+                        ],
+                        'attendees': [
+                            {'email': 'lpage@example.com'},
+                            {'email': 'sbrin@example.com'},
+                        ],
+                        'reminders': {
+                            'useDefault': False,
+                            'overrides': [
+                                {'method': 'email', 'minutes': 24 * 60},
+                                {'method': 'popup', 'minutes': 10},
+                            ],
+                        },
+                    }
+                    event = {
+                        'summary': show_name,
+                        'description': 'S: ' + season + 'E: ' + episode,
+                        'start': {
+                            'dateTime': datetime_object_start_str,
+                            'timeZone': 'America/Los_Angeles',
+                        },
+                        'end': {
+                            'dateTime': datetime_object_end_str,
+                            'timeZone': 'America/Los_Angeles',
+                        }
+                    }
 
-            self.print_calendar(calendar_day)
+                    self.all_events.append(event)
+
+        self.add_2_calendar(self.all_events)
 
     def get_time(self, show_id):
         show_page = requests.get(self.link + str(show_id))
@@ -69,15 +127,60 @@ class HTMLParser():
         am_pm = show_page_soup.find('td', class_='SimklTVAboutYearCountry').get_text().split()[6]
         if am_pm == 'PM':
             est_time_hour += 12
+        else:
+            est_time_hour = '0' + str(est_time_hour)
         return str(est_time_hour) + ':' + str(est_time_minute)
 
     def print_calendar(self, day):
         print (str(day.day) + "-" + str(day.month) + "-" + str(day.year) + " " + day.str_day)
         for s in day.today:
-            print s.title
-            print s.est_time
-            print "S:" + s.season + " E:" + s.episode
-            print "\n"
+            print (s.title)
+            print (s.est_time)
+            print ("S:" + s.season + " E:" + s.episode)
+            print ("\n")
+
+    def add_2_calendar(self, all_events):
+        # If modifying these scopes, delete the file token.pickle.
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+        for evnt in all_events:
+            if evnt not in events:
+                add_event = service.events().insert(calendarId='primary', body=evnt).execute()
+            else:
+                print('pas geciyom')
 
     def get_html(self):
 
