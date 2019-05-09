45f984b74e9e5b3df627ae6a9275f1ba9f8b316f
Deniz Gökçin <dgokcin@gmail.com>
2019-05-09 08:36:18 +0300

    add2calendar function is being called 12 times. thats the reason for duplicates

--- src/html_parser.py
+++ src/html_parser.py
@@ -78,35 +78,9 @@ class HTMLParser():
                     datetime_object_end_str = end_datetime.isoformat()
 
                     s = TVShow(show_name, season, episode, fixed_time)
-                    self.k = {
-                        'summary': 'Google I/O 2015',
-                        'location': '800 Howard St., San Francisco, CA 94103',
-                        'description': 'A chance to hear more about Google\'s developer products.',
-                        'start': {
-                            'dateTime': '2015-05-28T09:00:00-07:00',
-                            'timeZone': 'America/Los_Angeles',
-                        },
-                        'end': {
-                            'dateTime': '2015-05-28T17:00:00-07:00',
-                            'timeZone': 'America/Los_Angeles',
-                        },
-                        'recurrence': [
-                            'RRULE:FREQ=DAILY;COUNT=2'
-                        ],
-                        'attendees': [
-                            {'email': 'lpage@example.com'},
-                            {'email': 'sbrin@example.com'},
-                        ],
-                        'reminders': {
-                            'useDefault': False,
-                            'overrides': [
-                                {'method': 'email', 'minutes': 24 * 60},
-                                {'method': 'popup', 'minutes': 10},
-                            ],
-                        },
-                    }
+
                     event = {
-                        'summary': show_name + ' S: ' + season + 'E: ' + episode,
+                        'summary': show_name + ' S:' + season + 'E:' + episode,
                         'start': {
                             'dateTime': datetime_object_start_str,
                             'timeZone': 'America/New_York',
